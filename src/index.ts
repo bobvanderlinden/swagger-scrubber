2d8ac2fc855326c83879407eba2290ce11391f8d
Bob van der Linden <bobvanderlinden@gmail.com>
2018-05-17 11:24:15 +0200

    initial work to create cmdline tool

--- src/index.ts
+++ src/index.ts
@@ -1,91 +1,28 @@
-// import {
-//   validateDocument
-// } from './validation'
-// import {
-//   scrub
-// } from './scrub'
-
-// const swagger = require(process.argv[2])
-// const validationErrors = validateDocument(swagger)
-// const scrubbedSwagger = scrub(swagger)
-// for (let validationError of validationErrors) {
-//   console.log(validationError.message)
-// }
-// require('fs').writeFileSync('swagger.scrubbed.json', JSON.stringify(scrubbedSwagger, null, '  '))
-
+import * as yargs from 'yargs'
 import {
-  deleteJsonPath,
-  unique,
-  toObject,
-  equals
-} from './utils'
-import * as Converter from 'api-spec-converter'
+  validateAndScrubExhaustive
+} from './scrub'
 
-function isEmptyObject(obj) {
-  return obj.constructor === Object && Object.keys(obj).length === 0
-}
-
-function mapObject(obj, fn) {
-  return toObject(fn(Object.entries(obj)))
-}
-
-function removeEmptyObjects(obj) {
-  return mapObject(obj, entries => entries.filter(([key, value]) => !isEmptyObject(value)))
-}
-
-const totalErrors = []
-
-async function scrub(source) {
-  const converted = await Converter.convert({
-    from: 'swagger_2',
-    to: 'swagger_2',
-    source
+yargs
+  .usage('$0 <cmd> [args]')
+  .command('scrub [source]', 'remove invalid endpoints and definitions from a Swagger/OpenAPI specification', (yargs) => {
+    return yargs.positional('source', {
+        type: 'string',
+        describe: 'the file or URL where the Swagger/OpenAPI specification resides'
+      })
+      .option('from', {
+        alias: 'f',
+        description: 'the format of the Swagger specification',
+        choices: ['swagger_1', 'swagger_2', 'openapi_3'],
+        required: true
+      })
+  }, function ({source, from}) {
+    validateAndScrubExhaustive(source, {
+      from: from
+    }).then(result => {
+      process.stderr.write(JSON.stringify(result.validationErrors, null, '  '))
+      process.stdout.write(JSON.stringify(result.spec, null, '  '))
+    })
   })
-  const result = await converted.validate()
-  const json = JSON.parse(converted.stringify())
-
-  const errors = result.errors
-    .filter(error => error.code !== 'DUPLICATE_OPERATIONID')
-
-  totalErrors.push(...errors)
-
-  const paths = errors
-    .filter(error => error.path[0] === 'paths' && error.path.length >= 2)
-    .map(error => error.path.slice(0, 3))
-  const definitions = errors
-    .filter(error => error.path[0] === 'definitions' && error.path.length >= 2)
-    .map(error => error.path.slice(0, 2))
-
-  const scrubbed = unique(
-      paths.concat(definitions)
-        .map(path => JSON.stringify(path))
-    )
-    .map(path => JSON.parse(path))
-    .reduce((state, path) => deleteJsonPath(state, path), json)
-  
-  const scrubbed2 = {
-    ...scrubbed,
-    paths: removeEmptyObjects(
-      mapObject(scrubbed.paths, entries => entries.map(([key, value]) => [key, removeEmptyObjects(value)]))
-    ),
-    definitions: removeEmptyObjects(scrubbed.definitions)
-  }
-
-  return scrubbed2
-}
-
-async function keepScrubbing(source) {
-  while (true) {
-    const target = await scrub(source)
-    if (equals(source, target)) {
-      return target
-    } else {
-      source = target
-    }
-  }
-}
-
-keepScrubbing('api-development.ons.io.swagger.json').then(result => {
-  process.stdout.write(JSON.stringify(result, null, '  '))
-  process.stderr.write(JSON.stringify(totalErrors, null, '  '))
-})
+  .help()
+  .argv
--- src/scrub.ts
+++ src/scrub.ts
@@ -1,29 +1,79 @@
 import {
-  deleteJsonPath
+  deleteJsonPath,
+  unique,
+  toObject,
+  equals
 } from './utils'
-import {
-  validateDocument
-} from './validation'
+import * as Converter from 'api-spec-converter'
 
-export function scrub(swagger) {
-  let result = swagger
-  do {
-    swagger = result
-    const errors = validateDocument(result)
-    result = scrubErrors(result, errors)
-  } while (JSON.stringify(swagger) !== JSON.stringify(result));
-  return result
+function isEmptyObject(obj) {
+  return obj.constructor === Object && Object.keys(obj).length === 0
 }
 
-export function scrubErrors(swagger, validationErrors) {
-  return validationErrors.reduce((swagger, validationError) => {
-    switch (validationError.jsonPath[0]) {
-      case 'paths':
-        return deleteJsonPath(swagger, validationError.jsonPath.slice(0, 3))
-      case 'definitions':
-        return deleteJsonPath(swagger, validationError.jsonPath.slice(0, 2))
-      default:
-        return swagger
+function mapObject(obj, fn) {
+  return toObject(fn(Object.entries(obj)))
+}
+
+function removeEmptyObjects(obj) {
+  return mapObject(obj, entries => entries.filter(([key, value]) => !isEmptyObject(value)))
+}
+
+export async function scrub(source: any, validationErrors) {
+  const paths = validationErrors
+    .filter(error => error.path[0] === 'paths' && error.path.length >= 2)
+    .map(error => error.path.slice(0, 3))
+  const definitions = validationErrors
+    .filter(error => error.path[0] === 'definitions' && error.path.length >= 2)
+    .map(error => error.path.slice(0, 2))
+
+  const scrubbed = unique(
+      paths.concat(definitions)
+        .map(path => JSON.stringify(path))
+    )
+    .map(path => JSON.parse(path))
+    .reduce((json, path) => deleteJsonPath(json, path), source)
+  
+  return {
+    ...scrubbed,
+    paths: removeEmptyObjects(
+      mapObject(scrubbed.paths, entries => entries.map(([key, value]) => [key, removeEmptyObjects(value)]))
+    ),
+    definitions: removeEmptyObjects(scrubbed.definitions)
+  }
+}
+
+export async function validateAndScrub(source, options) {
+  const converted = await Converter.convert({
+    from: options.from,
+    to: 'swagger_2',
+    source
+  })
+  const result = await converted.validate()
+  const validationErrors = result.errors || []
+  const spec = JSON.parse(converted.stringify())
+  const scrubbedSpec = await scrub(spec, validationErrors)
+  return {
+    spec: scrubbedSpec,
+    validationErrors: validationErrors
+  }
+}
+
+export async function validateAndScrubExhaustive(spec, options) {
+  const validationErrors = []
+  let iteration = 0
+  do {
+    const result = await validateAndScrub(spec, options)
+    options = { ...options, from: 'swagger_2' }
+    if (result.validationErrors.length === 0) {
+      break
     }
-  }, swagger)
+    validationErrors.push(...result.validationErrors.map(error => ({ ...error, iteration })))
+    spec = result.spec
+    iteration++
+  } while (true);
+
+  return {
+    spec,
+    validationErrors
+  }
 }
